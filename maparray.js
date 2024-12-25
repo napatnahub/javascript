d8e2e9422d16d097b80ac70550b8434d0cb5ac16
napatnahub <napat.nahub@gmail.com>
2024-12-25 09:07:13 +0700

    update

---
+++ maparray.js
@@ -0,0 +1,5 @@
+const finalParticpants =["Taylor","Donald","Don","Natasha","Bobby"];
+const announcements = finalParticpants.map((member)=>{
+    return member +"joined the contest.";
+});
+console.log(announcements);
---
+++ population.js
@@ -0,0 +1,13 @@
+let arr1 =["A",tryue,2];
+console.log("\n********************************Length and index*******************\n");
+console.log(arr1.length);
+conlsoe.log(arr1[3]);
+conlsoe.log(arr1[2]);
+conlsoe.log(arr1[arr1.length-1]);
+consloe.log("\n******************Length and index part2*************\n");
+let arr3=Array(3);
+arr3[2]="Adding a value!";
+conlsoe.log(arr3[2]);
+conlsoe.log(arr3[0]);
+conlsoe.log(arr3[1]);
+conlsoe.log(arr3[arr3.length-1]);
